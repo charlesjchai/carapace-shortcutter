96a5ef89ad252d5f8bc175648aef651dfa3450b9
Charles Chai <charlesxiaoanchai@gmail.com>
2026-07-22 16:41:44 -0700

    making progress

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use anyhow::{Context, Result};
+use anyhow::{Context, Result, bail};
+use serde_json::{Map, Value};
 use std::fs::{File};
 use std::path::Path;
 use std::io::{BufReader, BufWriter, Read, Write};
-use std::env;
 
 fn main() -> Result<()> {
 
@@ -11,16 +11,23 @@ fn main() -> Result<()> {
 
     // Check if settings.json exists and create it if no
     if Path::try_exists(file_path)? {
-        file = File::open(file_path)?;
+        file = File::open(file_path)
+            .context(format!("\"{}\" could not be opened.",
+                file_path.file_name().unwrap().to_str().unwrap()))?;
     } else {
         file = File::create_new(file_path)?;
     }
     let reader = BufReader::new(&file);
-    let mut str_json: String = serde_json::from_reader(reader)
-        .context("Failed to read json")?;
-    if file.metadata()?.len() == 0 {
-        str_json.push_str("{}");
-    }
-
+    // Creates a mutable copy of settings.json
+    let json_map: Map<String, Value> = if file.metadata()?.len() == 0 {
+        serde_json::Map::new()
+    } else {
+        let json_temp: Value = serde_json::from_reader(reader)?;
+        match json_temp {
+            Value::Object(map) => map,
+            _ => bail!("JSON root is not an object"),
+        }
+    };
+    dbg!(json_map);
     Ok(())
 }
