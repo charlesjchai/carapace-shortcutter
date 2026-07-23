e5537714c3fed964ea0f66628c12cfe28b5a6f41
Charles Chai <charlesxiaoanchai@gmail.com>
2026-07-22 17:28:18 -0700

    Made area_err which formats errors

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
-use anyhow::{Context, Result, bail};
+use anyhow::{Context, Result};
+use clap::Parser;
 use serde_json::{Map, Value};
 use std::fs::{File};
 use std::path::Path;
 use std::io::{BufReader, BufWriter, Read, Write};
 
+// Formats an error message with its file, line and column
+macro_rules! area_err {
+    ($a:expr) => {
+        format!("\"{}\" at {}:{}:{}", $a, file!(), line!(), column!())
+    };
+}
 fn main() -> Result<()> {
 
     let file_path = Path::new("settings.json");
@@ -12,8 +19,8 @@ fn main() -> Result<()> {
     // Check if settings.json exists and create it if no
     if Path::try_exists(file_path)? {
         file = File::open(file_path)
-            .context(format!("\"{}\" could not be opened.",
-                file_path.file_name().unwrap().to_str().unwrap()))?;
+            .context(area_err!(format!("{} could not be opened",
+                file_path.file_name().unwrap().to_str().unwrap())))?;
     } else {
         file = File::create_new(file_path)?;
     }
@@ -22,12 +29,22 @@ fn main() -> Result<()> {
     let json_map: Map<String, Value> = if file.metadata()?.len() == 0 {
         serde_json::Map::new()
     } else {
-        let json_temp: Value = serde_json::from_reader(reader)?;
+        let json_temp: Value = serde_json::from_reader(reader)
+            .context(area_err!("Invalid syntax"))?;
         match json_temp {
             Value::Object(map) => map,
-            _ => bail!("JSON root is not an object"),
+            _ => panic!("{}", area_err!("JSON root is not an object")),
         }
     };
     dbg!(json_map);
     Ok(())
+}
+
+/// A shortcut tool that makes shortcuts of shell commands
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Add a shortcut
+    #[arg(short, long)]
+    add: String,
 }
