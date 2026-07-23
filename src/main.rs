3aaa7bffb54780ecf5b0292dc80387bcad7e491a
Charles Chai <charlesxiaoanchai@gmail.com>
2026-07-23 16:26:04 -0700

    Started script that initializes shell's rc file

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,65 @@
-use anyhow::{Context, Result};
+use anyhow::{Context, Result, bail};
 use clap::Parser;
 use serde_json::{Map, Value};
 use std::fs::{File};
 use std::path::Path;
-use std::io::{BufReader, BufWriter, Read, Write};
+use std::io::{self, BufReader, Write};
 
 #[cfg(not(target_family = "unix"))]
 compile_error!("WHAT THE HECK ARE YOU DOING TRYING TO COMPILE THIS ON A NON-UNIX SYSTEM???");
 
-/// Formats an error message with its file, line and column
+/// Formats an error message with its location (file, line and column)
+/// Do not use with functions that include an error's location
 macro_rules! area_err {
     ($a:expr) => {
         format!("\"{}\" at {}:{}:{}", $a, file!(), line!(), column!())
     };
 }
 fn main() -> Result<()> {
-    // Checks if system is not Unix
-    if cfg!(not(target_family = "unix")) {
-        panic!("WHAT THE HECK ARE YOU DOING TRYING TO RUN THIS ON A NON-UNIX SYSTEM???");
-    }
 
     let file_path = Path::new("settings.json");
     let file: File;
-
     // Check if settings.json exists and create it if no
     if Path::try_exists(file_path)? {
         file = File::open(file_path)
-            .context(area_err!(format!("{} could not be opened",
-                file_path.file_name().unwrap().to_str().unwrap())))?;
+            .context(area_err!("File could not be opened"))?;
     } else {
-        file = File::create_new(file_path)?;
+        file = File::create_new(file_path)
+            .context(area_err!("File could not be created"))?;
     }
     let reader = BufReader::new(&file);
-    // Creates a mutable copy of settings.json
-    let json_map: Map<String, Value> = if file.metadata()?.len() == 0 {
-        serde_json::Map::new()
+    let rc_path: &Path;
+
+    // Either creates a mutable copy of settings.json or initializes an empty one
+    let mut settings: Map<String, Value> = if file.metadata()?.len() == 0 {
+        Map::new()
     } else {
-        let json_temp: Value = serde_json::from_reader(reader)
-            .context(area_err!("Invalid syntax"))?;
+        let json_temp = serde_json::from_reader(reader)
+            .context(area_err!("Invalid JSON syntax"))?;
         match json_temp {
             Value::Object(map) => map,
-            _ => panic!("{}", area_err!("JSON root is not an object")),
+            _ => bail!(area_err!("JSON root is not an object")),
         }
     };
-    dbg!(json_map);
+    
+    // Asks for the shell's rc file if it isn't found
+    if settings.contains_key("shell_rc_file") {
+        rc_path = Path::new(&settings["shell_rc_file"].to_string());
+    } else {
+        print!("The shell's rc file was not specified. \n\
+        Please provide the path to it (~/.zshrc, ~/.bashrc), \
+        or type \"auto\" to find it automatically.\n\n\
+        Shell rc file path: ");
+        io::stdout().flush()?;
+        let mut rc_temp = String::new();
+        io::stdin()
+            .read_line(&mut rc_temp)
+            .context(area_err!("Failed to read rc file"))?;
+        rc_path = Path::new(&rc_temp);
+        settings.insert("shell_rc_file".to_string(), Value::String(rc_temp.trim().to_string()));
+    }
+    dbg!(&settings);
+    
     Ok(())
 }
 
@@ -52,6 +68,5 @@ fn main() -> Result<()> {
 #[command(version, about, long_about = None)]
 struct Args {
     /// Add a shortcut
-    #[arg(short, long)]
     add: String,
 }
