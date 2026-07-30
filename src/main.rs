f8cc1ff694c3ba6c6321a56fb46bac94ba196895
charlesjchai <charlesxiaoanchai@gmail.com>
2026-07-29 23:07:04 -0400

    Add aliases file and handling

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,37 @@
 use anyhow::{Context, Result};
 use clap::Parser;
 use serde_json::Value;
-use std::fs::File;
+use std::fs::{self, File};
 use std::io::{self, BufReader, BufWriter, Write};
-use std::path::{PathBuf};
+use std::path::{self, Path, PathBuf};
 
 #[cfg(not(target_family = "unix"))]
-compile_error!("Support for non-unix systems is not yet supported.");
+compile_error!("Non-Unix systems are not yet supported.");
 
 fn main() -> Result<()> {
-    assert!(
-        cfg!(target_family = "unix"),
-        "Support for non-unix systems is not yet supported."
-    );
-
-    let file_path = PathBuf::from("settings.json");
-    // Open the file and create it if it doesn't exist
-    let file = File::options()
+    let json_path = PathBuf::from("resources/settings.json");
+    let aliases_path = PathBuf::from("resources/aliases");
+    let json_file = File::options()
         .read(true)
         .write(true)
         .create(true)
-        .open(file_path.to_str().unwrap())?;
-    /*let mut file: File =  if Path::try_exists(file_path)? {
-        File::open(file_path).context(area_err!("File could not be opened"))?
-    } else {
-        File::create_new(file_path).context(area_err!("File could not be created"))?
-    };*/
-    let reader = BufReader::new(&file);
+        .open(json_path.to_str().unwrap())
+        .context(area_err!("Could not open settings.json"))?;
+    let aliases_file = File::options()
+        .read(true)
+        .append(true)
+        .create(true)
+        .open(json_path.to_str().unwrap())
+        .context(area_err!("Could not open `aliases` file"))?;
+    let reader = BufReader::new(&json_file);
 
     // Either creates a mutable copy of settings.json or initializes an empty one
-    let mut json_val = if file.metadata()?.len() == 0 {
+    let mut json_val = if json_file.metadata()?.len() == 0 {
         Value::Object(serde_json::Map::new())
     } else {
         serde_json::from_reader(reader).context(area_err!("Invalid syntax"))?
     };
-    let settings = json_val
+    let settings_json = json_val
         .as_object_mut()
         .context(area_err!("Not an object"))?;
     /*let mut settings: Map<String, Value> = if file.metadata()?.len() == 0 {
@@ -49,50 +46,76 @@ fn main() -> Result<()> {
     };*/
 
     // Asks for the shell's rc file if it isn't found
-    let rc_path: PathBuf;
+    let shellrc_path: PathBuf;
     let mut buf = String::new();
-    if settings.contains_key("shell_rc_file") {
-        rc_path = PathBuf::from(
-            settings["shell_rc_file"]
+    if settings_json.contains_key("shellrc_file") {
+        shellrc_path = PathBuf::from(
+            settings_json["shellrc_file"]
                 .as_str()
-                .context("Error reading JSON")?,
+                .context(area_err!("Error reading JSON"))?,
         );
     } else {
         print!(
             "The shell's rc file was not specified. \n\
-        Please provide the path to it (~/.zshrc, ~/.bashrc), \
+        Please provide the ABSOLUTE path to it ({}), \
         or type \"auto\" to find it automatically.\n\n\
-        Shell rc file path: "
+        > ",
+            if cfg!(target_os = "macos") {
+                "/Users/<user>/.bashrc"
+            } else {
+                "/home/<user>/.bashrc"
+            }
         );
         io::stdout().flush()?;
         io::stdin()
             .read_line(&mut buf)
             .context(area_err!("Failed to read rc file"))?;
-        rc_path = PathBuf::from(&buf);
-        settings.insert(
-            "shell_rc_file".to_string(),
+        shellrc_path = PathBuf::from(buf.trim());
+        settings_json.insert(
+            "shellrc_file".to_string(),
             Value::String(buf.trim().to_string()),
         );
     }
-    dbg!(&settings);
+    dbg!(&settings_json);
+    /*
 
     let shellrc_file = File::options()
         .read(true)
         .write(true)
         .append(true)
-        .open(rc_path);
+        .open(shellrc_path)
+        .context(area_err!("Could not open shellrc_file"))?;*/
+    dbg!(shellrc_path.to_str().unwrap());
 
-    // Truncate the file and quit
-    let file = File::create(file_path)?;
-    let mut writer = BufWriter::new(file);
-    quit(&mut writer, &json_val);
+    // Find if shell rc contains `aliases`
+    let aliases_temp = path::absolute(&aliases_path)?;
+    let aliases_path_str = aliases_temp.to_str().unwrap();
+    // let aliases_reg = Regex::new(&format!(r"\\n(\\s*\\{})", aliases_path_str))?;
+    let mut shellrc_file = File::options()
+        .append(true)
+        .create(true)
+        .open(&shellrc_path)
+        .context(area_err!("Shell rc file could not be opened"))?;
+    let shellrc_contents = fs::read_to_string(&shellrc_path).unwrap();
+    dbg!(&shellrc_contents);
+    if !shellrc_contents.contains(aliases_path_str) {
+        println!("`aliases` file not found in shell rc, inserting...");
+        writeln!(
+            shellrc_file,
+            "{}",
+            path::absolute(&aliases_path)?.to_str().unwrap()
+        )?;
+        dbg!(path::absolute(&aliases_path)?.to_str().unwrap());
+    }
+
+    quit(&json_path, &json_val);
 }
 
 /// Formats an error message with its location (file, line and column)
-/// Only use with anyhow error handling
+/// Only use with `anyhow::context`
 macro_rules! area_err {
     ($a:expr) => {
-        format!("\"{}\" at {}:{}:{}", $a, file!(), line!(), column!())
+        format!("[{}:{}:{}] {}", file!(), line!(), column!(), $a)
     };
 }
 pub(crate) use area_err;
@@ -105,7 +128,10 @@ struct Args {
     add: String,
 }
 
-fn quit(mut writer: impl std::io::Write, value: &Value) -> ! {
+fn quit(json_path: &Path, value: &Value) -> ! {
+    // Truncate the file and quit
+    let file = File::create(json_path).unwrap();
+    let mut writer = BufWriter::new(file);
     serde_json::to_writer_pretty(&mut writer, value).expect("Could not read Value");
     writer.flush().unwrap();
     std::process::exit(0);
